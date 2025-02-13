c3dce04748e7846bc50107c771e2a5b34cf77e86
Rub3nTercero <rubeneleven3@gmail.com>
2025-02-13 22:56:46 +0100

    Capitulo 3 Finalizado

--- Chapter 3/lista_invitados.py
+++ Chapter 3/lista_invitados.py
@@ -1,8 +1,33 @@
-# Autor Rubén Tercero - Fecha: 06/02/2025
-# Este script asigna personas a una lista de invitados
+# Autor Rubén Tercero - Fecha: 13/02/2025
+# Este script asigna, elimina e inserta personas a una lista de invitados
 
 invitados = ['arantxa', 'maria pombo', 'tom holland']
 
 print(f"Hola, te invito a cenar {invitados[0].title()}.")
 print(f"Hola, te invito a cenar {invitados[1].title()}.")
 print(f"Hola, te invito a cenar {invitados[2].title()}.")
+
+invitado_faltante = invitados.pop(1)
+print(f"Lamentablemente, {invitado_faltante.title()} no podra venir.")
+
+invitados.append("max")
+print(f"Hola, te invito a cenar {invitados[-1].title()}.")
+
+print("Hola, he encontrado una mesa más grande.")
+
+invitados.insert(0, "tara")
+invitados.insert(int(len(invitados)/2), "willy")
+invitados.append("willy wonka")
+
+for nombre in invitados:
+    print(f"Hola, {nombre.title()} te invito a la nueva fiesta. ¡Espero que te guste!")
+
+while len(invitados)>2:
+    nombre=invitados.pop()
+    print(f"Lo siento, {nombre.title()} pero no puedo invitarte a la fiesta.")
+    
+print(f"Hola, {invitados[0].title()} sigues invitado a mi fiesta.")
+print(f"Hola, {invitados[1].title()} sigues invitado a mi fiesta.")
+print(f"Total de invitados: {len(invitados)}")
+
+del(invitados)
---
+++ Chapter 3/mundo.py
@@ -0,0 +1,16 @@
+# Autor Rubén Tercero - Fecha: 13/02/2025
+# Este script prueba varios métodos para trabajar con listas
+lugares = ['londres', 'paris', 'roma', 'nueva york', 'praga']
+print(f"Lista original: {lugares}")
+print(f"Lista en orden alfabético: {sorted(lugares)}")
+print(f"Lista original: {lugares}")
+print(f"Lista en orden alfabético inverso: {sorted(lugares, reverse=True)}")
+print(f"Lista original: {lugares}")
+lugares.reverse()
+print(f"Lista inversa: {lugares}")
+lugares.reverse()
+print(f"Lista original: {lugares}")
+lugares.sort()
+print(f"Lista en orden alfabético: {lugares}")
+lugares.sort(reverse=True)
+print(f"Lista en orden alfabético inverso: {lugares}")
